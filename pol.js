35b0a6cada69b1984c0eb478ae59f36364606231
Leanurrahman <rahmanleanur@gmail.com>
2025-11-23 20:43:20 +0600

    initial commit

---
+++ pol.js
@@ -0,0 +1,11 @@
+
+
+// alert("Welcome to our website");
+// document.write("Leanur rahman</br>")
+//  document.write("Hello worls")
+
+// let name = "Leanur rahman";
+// document.write("My name is " + name);
+// document.getElementById("button").onclick = function() {
+//     alert("Message sent successfully");
+// };
